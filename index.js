7586318c43d48199af1854425977446a1a61d7db
Vladyslav Hrytsenko <vladgritsenko@gmail.com>
2020-06-27 15:05:21 +0300

    changes

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const fetch  = require('isomorphic-unfetch')
+const core   = require('@actions/core')
+const github = require('@actions/github')
 
-
-// most @actions toolkit packages have async methods
 async function run() {
-  try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+    try {
+        const ctx = github.context
+        const { owner, repo } = ctx.repo
+        const { eventName, sha, ref, workflow, actor, payload, runId } = ctx
+        const repoURL = `https://github.com/${owner}/${repo}`
+        const workflowURL = `${repoURL}/commit/${sha}/checks`
 
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+        const commitid = payload.head_commit.id.substring(0, 7)
+        const commiturl = payload.head_commit.url
+        const commitmsg = payload.head_commit.message
 
-    core.setOutput('time', new Date().toTimeString());
-  } 
-  catch (error) {
-    core.setFailed(error.message);
-  }
+        const url = core.getInput('url')
+        const state = core.getInput('state')
+        const icon = core.getInput('icon')
+        const data = {owner, repo, icon, sha, workflow, actor, state, commitid, commiturl, commitmsg, runId}
+
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: {'content-type': 'application/json'},
+            body: JSON.stringify(data)
+        })
+
+        if (!res.ok) {
+            throw new Error("Fetch: " + await res.text())
+        }
+
+        core.setOutput('finished', new Date().toTimeString());
+    }
+    catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
 run()
