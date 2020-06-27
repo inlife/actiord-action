dec72535e2cac15a1fbd48503973bfc9b7854e8f
Vladyslav Hrytsenko <vladgritsenko@gmail.com>
2020-06-27 15:32:43 +0300

    fixes 2

--- index.js
+++ index.js
@@ -19,15 +19,13 @@ async function run() {
         const icon = core.getInput('icon')
         const data = {owner, repo, icon, sha, workflow, actor, state, commitid, commiturl, commitmsg, runId}
 
+        console.log('sending data to', url)
+
         const res = await fetch(url, {
             method: 'POST',
             headers: {'content-type': 'application/json'},
             body: JSON.stringify(data)
         })
-
-        if (!res.ok) {
-            throw new Error("Fetch: " + await res.text())
-        }
     }
     catch (error) {
         core.setFailed(error.message);
