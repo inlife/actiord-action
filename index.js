bcb045ac08299253c9dac2311b900308de196178
Vladyslav Hrytsenko <vladgritsenko@gmail.com>
2021-04-22 09:45:42 +0300

    code: fixed issues

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ async function run() {
     try {
         const ctx = github.context
         const { owner, repo } = ctx.repo
-        const { eventName, sha, ref, workflow, actor, payload, runId } = ctx
-        const repoURL = `https://github.com/${owner}/${repo}`
-        const workflowURL = `${repoURL}/commit/${sha}/checks`
+        const { sha, workflow, actor, payload, runId } = ctx
+        // const repoURL = `https://github.com/${owner}/${repo}`
+        // const workflowURL = `${repoURL}/commit/${sha}/checks`
 
         const commitid = payload.head_commit.id.substring(0, 7)
         const commiturl = payload.head_commit.url
